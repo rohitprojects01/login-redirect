b4f335beaf2cbbceca3200256c2f21ef4b45d356
rohitprojects01 <147984268+rohitprojects01@users.noreply.github.com>
2023-10-17 17:00:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
+
+signUpButton.addEventListener("click", () => {
+  // Redirect to signup website
+  window.location.href = "https://www.marathitadkafoodie.online/";
+});
+
+signInButton.addEventListener("click", () => {
+  // Redirect to login website
+  window.location.href = "https://www.marathitadkafoodie.online/";
+});
